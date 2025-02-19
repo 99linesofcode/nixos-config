f5c4526b4c7b19f6e32fdb0141ba1f5f9fc24dc3
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-02-20 00:05:43 +0400

    feat(module): draft for yubikey support

---
+++ modules/yubikey.nix
@@ -0,0 +1,56 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.host.yubikey;
+in
+with lib;
+{
+  options = {
+    host.yubikey.enable = mkEnableOption "yubikey";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      yubikey-personalization
+      yubico-piv-tool
+      yubioath-flutter
+    ];
+
+    security = {
+      pam = {
+        # TODO: determine whether to enable or put behind enable flag
+        # sshAgentAuth.enable = true; # enable authenticating using SSH key instead of passwords
+        services = {
+          login.u2fAuth = true;
+          sudo.u2fAuth = true;
+        };
+      };
+    };
+
+    services = {
+      pcscd.enable = true;
+      udev = {
+        extraRules = ''
+          ACTION=="remove",\
+           ENV{ID_BUS}=="usb",\
+           ENV{ID_MODEL_ID}=="0407",\
+           ENV{ID_VENDOR_ID}=="1050",\
+           ENV{ID_VENDOR}=="Yubico",\
+           RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+        '';
+        packages = [ pkgs.yubikey-personalization ];
+      };
+    };
+
+    programs = {
+      gnupg.agent = {
+        enable = true;
+        enableSSHSupport = true;
+      };
+    };
+  };
+}
