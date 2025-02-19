3f9236ecc274e453493e07e4cf13571c486aa22b
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-02-20 00:15:31 +0400

    feat(module): nix specific settings such as gc and optimization

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -3,5 +3,6 @@
   imports = [
     ./home-manager.nix
     ./locale.nix
+    ./nix.nix
   ];
 }
---
+++ hosts/shared/nix.nix
@@ -0,0 +1,35 @@
+{ ... }:
+{
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "19:00";
+      persistent = true;
+      options = "--delete-older-than 14d";
+    };
+    settings = {
+      accept-flake-config = true;
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      log-lines = 30;
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+    };
+  };
+
+  nixpkgs = {
+    config = {
+      allowBroken = false;
+      allowUnfree = true;
+    };
+  };
+
+  system = {
+    stateVersion = "24.11";
+  };
+}
