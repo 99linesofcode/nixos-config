d8fe6e2b6175357611181e876e4e292fecd05132
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-02-20 00:05:06 +0400

    feat(module): low-level multimedia using pipewire and wireplumber

---
+++ modules/sound.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.host.sound;
+in
+with lib;
+{
+  options = {
+    host.sound.enable = mkEnableOption "sound with pipewire and wireplumber";
+  };
+
+  config = mkIf cfg.enable {
+    security = {
+      rtkit.enable = true;
+    };
+
+    services = {
+      pipewire = {
+        enable = true;
+        alsa = {
+          enable = true;
+          support32Bit = true;
+        };
+        pulse.enable = true;
+        wireplumber = {
+          enable = true;
+          extraConfig = {
+            "wireplumber.settings" = {
+              "device.routes.default-sink-volume" = 0.5;
+              "device.routes.default-source-volume" = 0.32;
+            };
+          };
+        };
+      };
+    };
+  };
+}
