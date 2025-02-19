e21f374e77a35a7cc9eb45a8538fddd2179c4de3
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-02-20 00:00:48 +0400

    feat(module): hyprland

---
+++ modules/hyprland.nix
@@ -0,0 +1,59 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.host.hyprland;
+in
+with lib;
+{
+  options = {
+    host.hyprland.enable = mkEnableOption "hyprland";
+  };
+
+  config = mkIf cfg.enable {
+    environment = {
+      sessionVariables.NIXOS_OZONE_WL = "1";
+    };
+
+    hardware = {
+      graphics = {
+        enable = true;
+        enable32Bit = true;
+      };
+    };
+
+    programs = {
+      hyprland = {
+        enable = true;
+        portalPackage = pkgs.xdg-desktop-portal-wlr;
+      };
+    };
+
+    security = {
+      pam = {
+        services.gdm.enableGnomeKeyring = true;
+      };
+    };
+
+    services = {
+      dbus.packages = [ pkgs.gcr ]; # required for pinentry-gnome3 to work
+      gnome.gnome-keyring.enable = true;
+      udisks2.enable = true; # allow udiskie to query and manipulate storage devices
+    };
+
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      config.common = {
+        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+      };
+      extraPortals = [
+        pkgs.xdg-desktop-portal-wlr
+        pkgs.xdg-desktop-portal-gtk
+      ];
+    };
+  };
+}
