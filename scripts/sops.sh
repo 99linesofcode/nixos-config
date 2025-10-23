4b0fa1c52c2578f06e2a96f5ef36d931d0826776
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-10-23 18:06:49 +0200

    refactor(sops): simplify sops scripts through code reuse and deduplication

--- scripts/sops.sh
+++ scripts/sops.sh
@@ -3,71 +3,46 @@
 sops_create_or_update_public_age_key() {
   anchor="$1"
   key="$2"
+  file="${3:-$PWD/.sops.yaml}"
 
-  if [ -n "$(yq ".keys[] | select(anchor == \"${anchor}\")" .sops.yaml)" ]; then
+  if [ -n "$(yq ".keys[] | select(anchor == \"${anchor}\")" "$file")" ]; then
     if [ "$VERBOSE" -eq 1 ]; then
       echo "Age key '${key}' for '${anchor}' was added before. Updating."
     fi
 
-    yq -i "(.keys[] | select(anchor == \"${anchor}\")) |= \"${key}\"" .sops.yaml
-    return
-  fi
+    yq -i "(.keys[] | select(anchor == \"${anchor}\")) |= \"${key}\"" "$file"
+  else
+    if [ "$VERBOSE" -eq 1 ]; then
+      echo "Adding new age key '${key}' for '${anchor}'."
+    fi
 
-  if [ "$VERBOSE" -eq 1 ]; then
-    echo "Adding new age key '${key}' for '${anchor}'."
+    yq -i ".keys += (\"${key}\" | . anchor = \"${anchor}\")" "$file"
   fi
-
-  yq -i ".keys += (\"${key}\" | . anchor = \"${anchor}\")" .sops.yaml
 }
 
-sops_add_host_creation_rules() {
+sops_create_or_update_creation_rules() {
   path="$1"
-  host="host_$(echo "$1" | awk -F'/' '{print $2}')"
+  anchor="${2:-host_$(echo "$1" | awk -F'/' '{print $2}')}"
+  file="${3:-$PWD/.sops.yaml}"
 
-  if [ -n "$(yq ".creation_rules[].path_regex | select(. == \"${path}\")" .sops.yaml)" ]; then
+  if [ -n "$(yq ".creation_rules[].path_regex | select(. == \"${path}\")" "$file")" ]; then
     if [ "$VERBOSE" -eq 1 ]; then
-      echo "creation_rules for '${host}' were already added before. Skipping."
+      echo "creation_rules for path_regex '${path}' were already added before. Updating."
     fi
 
-    return
-  fi
-
-  if [ "$VERBOSE" -eq 1 ]; then
-    echo "Adding new creation_rules for '${host}'."
-  fi
-
-  # hosts/shared/secrets/.*
-  yq -i "(.creation_rules[] | select(.path_regex == \"hosts/shared/secrets/.*\").key_groups[0].age) += (\"${host}\" | . alias |= .)" .sops.yaml
-
-  # hosts/${HOST}/secrets/.*
-  yq -i ".creation_rules += [{\"path_regex\": \"${path}\", \"key_groups\": [{\"age\": [(\"master\" | . alias |= .), (\"${host}\" | . alias |= .)]}]}]" .sops.yaml
-}
-
-sops_add_user_creation_rules() {
-  host="$1"
-
-  # users/${user}/secrets/.*
-  find ./users -mindepth 1 -maxdepth 1 -type d ! -name ".*" -printf '%f\n' >tmp
-
-  while IFS= read -r user; do
-    if [ -n "$(yq ".creation_rules[] | select(.path_regex == \"users/${user}/secrets/.*\").key_groups[0].age[] | select(alias == \"${host}\")" .sops.yaml)" ]; then
-      if [ "$VERBOSE" -eq 1 ]; then
-        echo "creation_rules for '${host}' for 'user_${user}' were already added before. Skipping."
-      fi
-
-      continue
-    fi
-
+    yq -i "(.creation_rules[] | select(.path_regex == \"${path}\").key_groups[0].age) += (\"${anchor}\" | . alias |= .)" "$file"
+  else
     if [ "$VERBOSE" -eq 1 ]; then
-      echo "Adding new creation_rules for '${host}' to 'user_${user}.'"
+      echo "Adding new creation_rules for path_regex '${path}'."
     fi
 
-    yq -i "(.creation_rules[] | select(.path_regex == \"users/${user}/secrets/.*\").key_groups[0].age) += (\"${host}\" | . alias |= .)" .sops.yaml
-  done <tmp
-
-  rm tmp
+    yq -i ".creation_rules += [{\"path_regex\": \"${path}\", \"key_groups\": [{\"age\": [(\"master\" | . alias |= .), (\"${anchor}\" | . alias |= .)]}]}]" "$file"
+  fi
 }
 
 sops_rekey() {
-  find . -type f -path "*/secrets/*" -exec sops updatekeys -y {} \;
+  file="${1:-$PWD}"
+  dir=${file%/*}
+
+  find "$dir" -type f -path "*/secrets/*" -exec sops --config "$dir/.sops.yaml" updatekeys -y {} \;
 }
