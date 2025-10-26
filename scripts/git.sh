e7d4728655b960abb1c4ae8c8977b7257b90a591
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-10-26 18:59:15 +0100

    fix(deploy): commit changes to current branch instead
    Committing the changes to another branch and switching back means the
    newly added keys are not present in the .sops.yaml file in the branch
    that we are working in.
    
    There isn't really a need to push the changes to another branch or the
    remote. I'd rather commit changes into the host branch I'm currently
    working on so as to keep these changes together in their own PR.

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,21 +1,14 @@
 #!/usr/bin/env sh
 
 cmd_git() {
-  remote="$(git symbolic-ref refs/remotes/origin/HEAD)"
   stash_label="$(od -An -N8 -tx1 /dev/urandom | tr -d ' \n')"
 
-  main_branch="${remote##*/}"
-  current_branch="$(git branch --show-current)"
-
   git stash -m "$stash_label"
-  git checkout "$main_branch"
 
   "$@"
 
   git add .
   git commit -m "chore(sops): generated and rekeyed secrets for host_${HOST}"
-  git push
 
-  git checkout "$current_branch"
   git stash list | grep "${stash_label}" && git stash pop --index
 }
