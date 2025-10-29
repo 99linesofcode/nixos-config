55b2fd7d93f536d3219ed0709a7145c96f12a071
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-10-29 14:48:16 +0000

    fix(rclone): create /var/lib/docker-plugins directories if NOT in rootless mode

--- modules/rclone.nix
+++ modules/rclone.nix
@@ -33,7 +33,7 @@ with lib;
     };
 
     systemd.tmpfiles.rules =
-      mkIf (config.host.docker.enable && config.host.docker.rootless.enable && dockerHasRclonePlugin)
+      mkIf (config.host.docker.enable && !config.host.docker.rootless.enable && dockerHasRclonePlugin)
         [
           "d /var/lib/docker-plugins/rclone/config 0755 root root -"
           "d /var/lib/docker-plugins/rclone/cache  0755 root root -"
