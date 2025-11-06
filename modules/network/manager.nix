819208d09ea01a51c6ff3274629cedd05cc52e1b
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-11-07 00:38:05 +0100

    fix(dns): use ipv4 DNS servers for the time being
    While I've not disabled IPv6 I noticed that some domain names being hit
    on their v6 address were not resolving (looking at you pecl.php.net you
    ancient beast). This seemed to resolve that issue for the time being.
    
    To be further researched..

--- modules/network/manager.nix
+++ modules/network/manager.nix
@@ -22,8 +22,10 @@ with lib;
       nameservers = [
         "9.9.9.9"
         "149.112.112.112"
-        "2620:fe::fe"
-        "2620:fe::9"
+        "1.1.1.1"
+        "1.0.0.1"
+        "8.8.8.8"
+        "8.8.4.4"
       ];
       resolvconf.enable = false;
       stevenblack.enable = true; # stevenblack hosts file blocklist
