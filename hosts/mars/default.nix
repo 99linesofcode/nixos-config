a07a17d70dca22a1c1315cd349e00129c5898a1a
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2026-01-29 14:38:46 +0000

    fix(restic): correctly define backup paths

--- hosts/mars/default.nix
+++ hosts/mars/default.nix
@@ -42,7 +42,7 @@ in
   };
 
   services = {
-    restic.paths = [
+    restic.backups.remotebackup.paths = [
       "/home/shorty/.config/server01/"
       "/home/shorty/.config/piratenportaal/"
       "/var/lib/docker/volumes"
