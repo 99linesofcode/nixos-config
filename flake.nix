df6a26ac8e80d2df4a8526c452411afc989e0cef
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2026-06-13 01:11:56 +0200

    chore(nix): we're now stable and can lock to 26.05

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
 
   # NOTE: nix version is manually kept in sync with home-manager
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     impermanence.url = "github:nix-community/impermanence";
     sops-nix = {
       url = "github:Mic92/sops-nix";
@@ -29,7 +29,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
